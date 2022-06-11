faa2ab71f7db002799abec6e5c75b84689dd0421
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-11 20:07:21 +0530

    Create Linklist.java

---
+++ Linklist.java
@@ -0,0 +1,19 @@
+/*
+ link list - creation !! implementation
+ */
+import java.io.*;
+
+class LinkedList {
+    Node head; // head of list
+    /* Linked list Node*/
+    class Node {
+        int data;
+        Node next;
+        // Constructor to create a new node Next is by default initialized as null
+        Node(int d) 
+        { 
+            data = d; 
+        }
+    }
+}
+
