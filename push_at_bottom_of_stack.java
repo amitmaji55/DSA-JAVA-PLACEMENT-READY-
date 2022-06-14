734eaa6ea88ea4461bb01f91f81dbd1d8ef6c998
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-14 22:02:04 +0530

    Create push_at_bottom_of_stack.java

---
+++ push_at_bottom_of_stack.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Push_bottom_of_stack{
+    public static void push_at_bottom(Stack<Integer> s, int data)
+    {
+       if(s.isEmpty())
+       {
+           s.push(data);
+           return;
+       }
+       int temp = s.pop();
+       push_at_bottom(s,data);
+       s.push(temp);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.add(10);
+        s.add(20);
+        s.add(30);
+        s.add(40);
+        push_at_bottom(s,60);
+        while(!s.isEmpty())
+        {
+            System.out.println(s.pop());
+        }
+
+    }
+}
