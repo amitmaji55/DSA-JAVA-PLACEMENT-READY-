67457d3909177b90539e6c03048b93aa7273a6c7
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-22 22:46:26 +0530

    Create BinarySearch.java

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,24 @@
+ class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0,16, 18, 22};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+           int mid = (start + end) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
