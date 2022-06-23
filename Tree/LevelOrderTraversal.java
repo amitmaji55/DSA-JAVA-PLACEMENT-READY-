a33b64e0de27d7ea02aaa4458e419c777d9252fa
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-23 21:08:40 +0530

    Update and rename LevelOrderTraversal.java.txt to LevelOrderTraversal.java

---
+++ Tree/LevelOrderTraversal.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> levelOrder(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<Integer> wrapList = new List<Integer>();
+        
+        if(root == null) return wrapList;
+        
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            
+            if(queue.peek().left != null) 
+                queue.offer(queue.peek().left);
+                
+            if(queue.peek().right != null) 
+                queue.offer(queue.peek().right);
+                
+            wrapList.add(queue.poll().val);
+        }
+        return wrapList;
+    }
+}
+
+
+/*
+Time Complexity: O(N)
+
+Space Complexity: O(N)
+*/
